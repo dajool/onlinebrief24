a78b1c1e3e24ec18d5ca6ad3b75291f11d85b2d5
Jochen Breuer <breuer@dajool.com>
2015-10-29 22:39:45 +0100

    Proof of concept
    With this code it is possible to upload to an sftp server.

--- onlinebrief24/client.py
+++ onlinebrief24/client.py
@@ -1,33 +1,58 @@
+# -*- coding: utf-8 -*-
+
 import paramiko
 
+
 class Client(object):
+    """\
+    Client for OnlineBrief24.de.
+    """
+
     def __init__(self, username, password):
+        self.host = "localhost"
+        self.port = 22
         self.username = username
         self.password = password
+        self.upload_path = "/home/breuer/foo"
         self.sftp = None
 
+    def __enter__(self):
+        """\
+        Enter the with-statement and return self so that it can be used as 'as'.
+        """
+        self.login()
+        return self
 
-    def login():
-        # todo: login to the ssh server with paramiko
-        # transport = paramiko.Transport((myhost, 22))
-        # transport.connect(username = self.username, password = self.password)
-        # self.sftp = paramiko.SFTPClient.from_transport(transport)
+    def __exit__(self, type, value, traceback):
+        """\
+        Exit the with-statement and disconnect from the sftp server.
+        """
+        self.disconnect()
+
+
+    def login(self):
+        """\
+        Login to the sftp server.
+        """
+        transport = paramiko.Transport((self.host, self.port))
+        transport.connect(username = self.username, password = self.password)
+        self.sftp = paramiko.SFTPClient.from_transport(transport)
+
+
+    def upload(self, path_to_file):
+        """\
+        Upload file to the sftp server.
+        Possible kwargs:
+           * color
+           * duplex
+           * envelope
+           * distribution
+           * registered
+        """
+        self.sftp.put(path_to_file, self.upload_path)
         pass
 
 
-    def upload(path_to_file):
-        # todo: upload file to server
-        # possible kwargs:
-        #    * color
-        #    * duplex
-        #    * envelope
-        #    * distribution
-        #    * registered
-        #
-        # self.sftp.put(path_to_file, remote_path)
-        pass
-
-
-    def disconnect():
+    def disconnect(self):
         self.sftp.close()
 

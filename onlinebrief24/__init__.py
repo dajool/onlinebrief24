b7e2c780d3a2166fa99c1ac561bafad8d3259dc7
Jochen Breuer <brejoc@gmail.com>
2019-04-02 14:34:57 +0200

    Adds version dunder to module

--- onlinebrief24/__init__.py
+++ onlinebrief24/__init__.py
@@ -1,1 +1,11 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+__project__ = 'onlinebrief24'
+
+try:
+    __version__ = get_distribution(__project__).version
+except DistributionNotFound:
+    __version__ = None
+
+
 from onlinebrief24.client import Client

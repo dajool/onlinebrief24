e78d56ad48915c6ed8bcaf99b1cd3f76edbf78ea
Jochen Breuer <brejoc@gmail.com>
2019-04-03 21:10:50 +0200

    Version bump to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='onlinebrief24',
-    version='1.0',
+    version='1.1',
     packages=find_packages("."),
     include_package_data=True,
     url='https://github.com/dajool/onlinebrief24',
